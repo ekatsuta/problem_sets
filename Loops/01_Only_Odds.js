d3c95dae2dfe548ee91c321b7e214597234dfc1d
Emi Katsuta <emi.katsuta@gmail.com>
2019-02-17 17:20:14 -0500

    finished problem

--- Loops/01_Only_Odds.js
+++ Loops/01_Only_Odds.js
@@ -1,7 +1,18 @@
+//Define a function, onlyOdds, that accepts a number as an argument. onlyOdds should should return the sum of all the odd numbers between the given number and 1.
+//If onlyOdds receives an argument less than 1, it should return 0.
+
 let onlyOdds = (num) => {
-  if (num >= 1) {
-    for (let i = num; i >= 1; i--) {
-      
+  let sum = 0;
+    if (num >= 1) {
+      for (let i = num; i >= 1; i--) {
+        if (i % 2 !== 0) {
+        sum += i;
+      } else {
+        continue;
+      }
+      }
+    } else {
+      return 0;
     }
-  }
+  return sum;
 }
